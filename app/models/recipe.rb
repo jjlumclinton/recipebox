198448d40f65de690d85ee254c29ae2a5cbe21a3
Josh McLinton <jjlumclinton@gmail.com>
2018-07-04 00:10:03 +0100

    Add user authentication

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -13,4 +13,6 @@ class Recipe < ApplicationRecord
   validates :title, :description, :image, presence: true
   has_attached_file :image, styles: { medium: "400x400#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  belongs_to :user
 end
